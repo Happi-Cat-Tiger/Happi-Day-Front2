09f0f2ffd899b186495cb4f33c628088b21e759d
성은지 <96868722+eunji1@users.noreply.github.com>
2023-12-19 13:58:42 +0900

    ADD: PrimaryButton.tsx FEAT: breackpoint: md: 사이즈 조절, hover, disable

---
+++ src/components/Button/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface PrimaryButtonProps {
+  label: string;
+  disabled?: boolean;
+  onClick: () => void;
+}
+const PrimaryButton = ({ label, disabled, onClick }: PrimaryButtonProps) => {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={disabled}
+      className="px-5 py-3 prose-btn-M text-white bg-orange2 rounded-2xl hover:bg-orange1 disabled:bg-gray6 md:px-6 md:py-4 md:prose-btn-L">
+      {label}
+    </button>
+  );
+};
+
+export default PrimaryButton;
