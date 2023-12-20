c5955982c7f92e1f8379a9203e40f96feab7b6f0
sungdongyoon <ehdtjd0906@naver.com>
2023-12-21 01:32:39 +0900

    메인페이지 UI 작업 및 카드 컴포넌트 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,106 +1,72 @@
-import Image from 'next/image';
-import logo from '../../public/images/logo.png';
+import Card from '@/components/Card';
 
 export default function Home() {
   return (
     <div className="my-10 h-auto w-screen">
-      <div className="m-auto flex flex-col gap-16 md:max-w-[1280px]">
-        <div className="h-[200px] bg-orange-100 md:max-w-[1280px]">구독 배너</div>
-        <div className="flex h-[386px] flex-col justify-between">
+      <div className="m-auto flex flex-col gap-16 md:max-w-[1024px]">
+        <div className="h-[200px] bg-orange-100 md:max-w-[1024px]">구독 배너</div>
+        <div className="flex h-[386px] flex-col justify-between gap-[35px]">
           <div className="flex justify-between">
-            <h2 className="prose-h4 border-b-[3px] border-orange2 p-2">인기 이벤트</h2>
+            <h2 className="prose-h3 border-b-[3px] border-orange2 p-[8px]">인기 이벤트</h2>
             <button>More</button>
           </div>
           <div className="flex justify-between">
-            <div className="flex h-[300px] w-[224px] cursor-pointer flex-col gap-[12px] p-[12px] shadow-xl">
-              <Image src={logo} alt="card" className="flex-1 bg-red-200" />
-              <div className="flex flex-1 flex-col items-center justify-center gap-1 border-t-[1px] border-black">
-                <span className="prose-h6">방탄소년단 생일 카페</span>
-                <span className="prose-body-S text-orange2">방탄소년단</span>
-                <span className="prose-body-S">용산 슈퍼스타 떡볶이</span>
-                <span className="prose-body-XS">2023.12.04 ~ 2023.12.05</span>
-                <div className="flex w-full flex-row justify-between text-gray5">
-                  <span className="prose-body-XXS">서울시 용산구</span>
-                  <div className="prose-body-XXS flex gap-[8px]">
-                    <span>10</span>
-                    <span>50</span>
-                    <span>100</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex h-[300px] w-[224px] cursor-pointer flex-col gap-[12px] p-[12px] shadow-xl">
-              <Image src={logo} alt="card" className="flex-1 bg-red-200" />
-              <div className="flex flex-1 flex-col items-center justify-center gap-1 border-t-[1px] border-black">
-                <span className="prose-h6">방탄소년단 생일 카페</span>
-                <span className="prose-body-S text-orange2">방탄소년단</span>
-                <span className="prose-body-S">용산 슈퍼스타 떡볶이</span>
-                <span className="prose-body-XS">2023.12.04 ~ 2023.12.05</span>
-                <div className="flex w-full flex-row justify-between text-gray5">
-                  <span className="prose-body-XXS">서울시 용산구</span>
-                  <div className="prose-body-XXS flex gap-[8px]">
-                    <span>10</span>
-                    <span>50</span>
-                    <span>100</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex h-[300px] w-[224px] cursor-pointer flex-col gap-[12px] p-[12px] shadow-xl">
-              <Image src={logo} alt="card" className="flex-1 bg-red-200" />
-              <div className="flex flex-1 flex-col items-center justify-center gap-1 border-t-[1px] border-black">
-                <span className="prose-h6">방탄소년단 생일 카페</span>
-                <span className="prose-body-S text-orange2">방탄소년단</span>
-                <span className="prose-body-S">용산 슈퍼스타 떡볶이</span>
-                <span className="prose-body-XS">2023.12.04 ~ 2023.12.05</span>
-                <div className="flex w-full flex-row justify-between text-gray5">
-                  <span className="prose-body-XXS">서울시 용산구</span>
-                  <div className="prose-body-XXS flex gap-[8px]">
-                    <span>10</span>
-                    <span>50</span>
-                    <span>100</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="flex h-[300px] w-[224px] cursor-pointer flex-col gap-[12px] p-[12px] shadow-xl">
-              <Image src={logo} alt="card" className="flex-1 bg-red-200" />
-              <div className="flex flex-1 flex-col items-center justify-center gap-1 border-t-[1px] border-black">
-                <span className="prose-h6">방탄소년단 생일 카페</span>
-                <span className="prose-body-S text-orange2">방탄소년단</span>
-                <span className="prose-body-S">용산 슈퍼스타 떡볶이</span>
-                <span className="prose-body-XS">2023.12.04 ~ 2023.12.05</span>
-                <div className="flex w-full flex-row justify-between text-gray5">
-                  <span className="prose-body-XXS">서울시 용산구</span>
-                  <div className="prose-body-XXS flex gap-[8px]">
-                    <span>10</span>
-                    <span>50</span>
-                    <span>100</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="flex h-[386px] flex-col justify-between">
-          <div className="flex justify-between">
-            <h2 className="prose-h4 border-b-[3px] border-orange2 p-2">인기 굿즈</h2>
-            <button>More</button>
-          </div>
-          <div className="flex justify-between">
-            <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
+            <Card
+              cardType={'event'}
+              cardTitle={'방탄소년단 생일 카페'}
+              cardArt={'방탄소년단'}
+              cardLocation={'용산 슈퍼스타 떡볶이'}
+              cardStart={'2023.12.04'}
+              cardEnd={'2023.12.05'}
+              cardAddress={'서울시 용산구'}
+              cardLike={5}
+              cardComments={3}
+              cardView={7}
+            />
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
           </div>
         </div>
-        <div className="flex h-[386px] flex-col justify-between">
+        <div className="flex h-[386px] flex-col justify-between gap-[35px]">
           <div className="flex justify-between">
-            <h2 className="prose-h4 border-b-[3px] border-orange2 p-2">인기 공구</h2>
+            <h2 className="prose-h3 border-b-[3px] border-orange2 p-[8px]">인기 굿즈</h2>
             <button>More</button>
           </div>
           <div className="flex justify-between">
+            <Card
+              cardType={'goods'}
+              cardTitle={'정국 포토카드'}
+              cardArt={'방탄소년단'}
+              cardStart={'2023.12.04'}
+              cardEnd={'2023.12.05'}
+              joinMember={55}
+              cardLike={5}
+              cardComments={3}
+              cardView={7}
+            />
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
+            <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
+            <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
+          </div>
+        </div>
+        <div className="flex h-[386px] flex-col justify-between gap-[35px]">
+          <div className="flex justify-between">
+            <h2 className="prose-h3 border-b-[3px] border-orange2 p-[8px]">인기 공구</h2>
+            <button>More</button>
+          </div>
+          <div className="flex justify-between">
+            <Card
+              cardType={'sales'}
+              cardTitle={'카리나 인형 키링'}
+              cardArt={'에스파'}
+              cardStart={'2023.12.04'}
+              cardEnd={'2023.12.05'}
+              joinMember={30}
+              cardLike={5}
+              cardComments={3}
+              cardView={7}
+            />
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
             <div className="h-[300px] w-[224px] border-2 border-black">카드</div>
