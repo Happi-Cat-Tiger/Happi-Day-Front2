2cc64553134ed7ea0873266343f24337621c48a1
성은지 <96868722+eunji1@users.noreply.github.com>
2023-12-20 14:47:01 +0900

    CHORE: css 수정

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -2,9 +2,8 @@ export default function BoardLayout({ children }: { children: React.ReactNode })
   return (
     <section>
       {/* Include shared UI here e.g. a header or sidebar */}
-      <nav className="h-[62px] bg-gray4">네브바</nav>
       <div className="flex justify-center items-center mx-auto md:max-w-[1280px] border-gray1 border-solid border-2">
-        <div className="h-screen px-2">
+        <div className="h-full min-h-screen px-2">
           <div className="w-full">{children}</div>
         </div>
       </div>
