962000a45c36613839f50dd9d0dee160ecca0f67
성은지 <96868722+eunji1@users.noreply.github.com>
2023-12-26 14:46:53 +0900

    ADD: react.quill 간단 사용 - CustomEditor.tsx - tmpEditor/page.tsx

---
+++ src/components/Tool/CustomEditor.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React, { useState, useMemo } from 'react';
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css';
+
+const CustomEditor = ({ initialData }: { initialData: string }) => {
+  const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
+
+  const [editValue, setEditValue] = useState(initialData);
+
+  const modules = {
+    toolbar: [
+      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+
+      [{ header: 1 }, { header: 2 }], // custom button values
+      [{ header: [1, 2, 3, 4, 5, 6, false] }], // font size
+      [{ list: 'ordered' }, { list: 'bullet' }],
+
+      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+      [{ align: [] }],
+      ['link', 'image'],
+    ],
+  };
+
+  const formats = [
+    'header',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'list',
+    'bullet',
+    'link',
+    'image',
+    'color',
+    'background',
+    'size',
+    'align',
+  ];
+  return (
+    <ReactQuill
+      theme="snow"
+      value={editValue}
+      onChange={setEditValue}
+      modules={modules}
+      formats={formats}
+      className=" w-3/4 h-[600px] mt-4 mx-auto"
+    />
+  );
+};
+
+export default CustomEditor;
