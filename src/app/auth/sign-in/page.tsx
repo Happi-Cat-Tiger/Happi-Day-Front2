0156e0d2f568aaaf9ea87d6cb8e98c198c3fb3fd
이지혜 <ehckehck@gmail.com>
2023-12-27 18:08:57 +0900

    ADD : sign-in UI

---
+++ src/app/auth/sign-in/page.tsx
@@ -0,0 +1,30 @@
+import Footer from '@/components/Footer/Footer';
+import AuthInput from '@/components/Auth/AuthInput';
+import { AiOutlineUser, AiOutlineLock } from 'react-icons/ai';
+
+export default function RootLayout() {
+  const inputPlaceHolder = [
+    { title: '', icon: AiOutlineUser, content: 'example@happiday.com' },
+    { title: '', icon: AiOutlineLock, content: '비밀번호' },
+  ];
+
+  return (
+    <>
+      <div className="pt-20 px-2 md:gap-10 flex w-[360px] items-start gap-[37px] pb-[130px] md:py-[120px]">
+        <div className="prose-h4 text-gray1 md:prose-h3">로그인</div>
+        <AuthInput inputPlaceHolder={inputPlaceHolder} />
+        <div className="flex-column md:gap-10 flex items-center gap-[33px] self-stretch">
+          <div className="gap-3 prose-body-XS flex items-center text-gray1">
+            <div>이메일 찾기</div>
+            <div>|</div>
+            <div>비밀번호 찾기</div>
+            <div>|</div>
+            <div>회원가입</div>
+          </div>
+          <button>로그인</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
