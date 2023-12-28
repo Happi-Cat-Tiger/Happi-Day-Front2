c888d3ffa3358f0c97bb5edeaf63e1166b34b3fb
성은지 <96868722+eunji1@users.noreply.github.com>
2023-12-29 01:42:46 +0900

    refactor: 컴포넌트 구분 - ImageUploader, HashtagInput, ThumbnailUploader move: form -> writeForm

--- src/app/board/write/page.tsx
+++ src/app/board/write/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 import StepProgressBar from '@/components/Bar/StepProgressBar';
 import PrimaryButton from '@/components/Button/PrimaryButton';
+import DaumPost from '@/components/Tool/DaumPost';
+import ImageUploader from '@/components/Tool/ImageUploader';
+import AddressInput from '@/components/WriteForm/AddressInput';
+import HashtagInput from '@/components/WriteForm/HashtagInput';
+import ThumbnailUploader from '@/components/WriteForm/ThumbnailUploader';
 import { useState } from 'react';
 
 export default function WritePage() {
   const [step, setStep] = useState<number>(1);
+
   return (
     <h1>
       Hello, WritePage!
@@ -12,17 +18,27 @@ export default function WritePage() {
       <button onClick={() => setStep(1)}>reset</button>
       <section className="flex w-screen max-w-[996px] flex-col items-center gap-4">
         <StepProgressBar step={step} />
-        <div className="flex w-full justify-end">
+        <div className="flex w-full justify-between">
+          <PrimaryButton label="이전" disabled={step <= 1} onClick={() => setStep(step - 1)} />
           <PrimaryButton label="다음" onClick={() => setStep(step + 1)} />
         </div>
-        <div className="h-[660px] w-full border border-gray-200 p-4">
-          <input
-            type="text"
-            className="w-full rounded-md border border-gray3 px-1.5 py-1 focus:border-orange1 focus:outline-none "
-            placeholder="제목"
-          />
-          <div className="h-full w-full bg-gray-100">에디터 자리</div>
-        </div>
+        {step === 1 && (
+          <div className="h-[660px] w-full border border-gray-200 p-4">
+            <input
+              type="text"
+              className="w-full rounded-md border border-gray3 px-1.5 py-1 focus:border-orange1 focus:outline-none "
+              placeholder="제목"
+            />
+            <div className="h-full w-full bg-gray-100">에디터 자리</div>
+          </div>
+        )}
+        {step === 2 && (
+          <div className="flex h-[660px] w-full flex-col gap-4 border border-gray-200 p-4">
+            <HashtagInput />
+            <ThumbnailUploader />
+            <AddressInput />
+          </div>
+        )}
       </section>
     </h1>
   );
---
+++ src/components/WriteForm/ThumbnailUploader.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ImageUploader from '../Tool/ImageUploader';
+
+const ThumbnailUploader = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <div className=" prose-h5">
+        <span>썸네일 사진 등록</span> <span className="text-red-600">*</span>
+      </div>
+      <ImageUploader />
+    </div>
+  );
+};
+
+export default ThumbnailUploader;
