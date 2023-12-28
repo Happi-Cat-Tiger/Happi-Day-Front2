25d2777254bd9d4aadb14f3acdb57740411d5494
성은지 <96868722+eunji1@users.noreply.github.com>
2023-12-28 14:25:06 +0900

    feature : stepProgressBar 단계별 css 입히기

--- src/app/board/write/page.tsx
+++ src/app/board/write/page.tsx
@@ -1,3 +1,29 @@
+'use client';
+import StepProgressBar from '@/components/Bar/StepProgressBar';
+import PrimaryButton from '@/components/Button/PrimaryButton';
+import { useState } from 'react';
+
 export default function WritePage() {
-  return <h1>Hello, WritePage!</h1>;
+  const [step, setStep] = useState<number>(1);
+  return (
+    <h1>
+      Hello, WritePage!
+      {step}
+      <button onClick={() => setStep(1)}>reset</button>
+      <section className="flex w-screen max-w-[996px] flex-col items-center gap-4">
+        <StepProgressBar step={step} />
+        <div className="flex w-full justify-end">
+          <PrimaryButton label="다음" onClick={() => setStep(step + 1)} />
+        </div>
+        <div className="h-[660px] w-full border border-gray-200 p-4">
+          <input
+            type="text"
+            className="w-full rounded-md border border-gray3 px-1.5 py-1 focus:border-orange1 focus:outline-none "
+            placeholder="제목"
+          />
+          <div className="h-full w-full bg-gray-100">에디터 자리</div>
+        </div>
+      </section>
+    </h1>
+  );
 }
--- src/components/Bar/StepProgressBar.tsx
+++ src/components/Bar/StepProgressBar.tsx
@@ -1,27 +1,39 @@
 import React from 'react';
-
-const StepProgressBar = () => {
+import { AiOutlineCheck } from 'react-icons/ai';
+interface StepProgressBarProps {
+  step: number;
+}
+const StepProgressBar = ({ step }: StepProgressBarProps) => {
   return (
     <>
       <div className="flex w-[664px] items-center">
         <div className="flex h-10 w-10 items-center justify-center rounded-full border border-orange2 text-orange2">
-          1
+          {step <= 1 ? 1 : <AiOutlineCheck />}
         </div>
-        <hr className="grow"></hr>
-        <div className="flex h-10 w-10 items-center justify-center rounded-full border border-gray6 text-gray6">2</div>
-        <hr className="grow"></hr>
-        <div className="flex h-10 w-10 items-center justify-center rounded-full border border-gray6 text-gray6">3</div>
+        <hr className={`h-[1px] grow border-0 ${step < 2 ? 'bg-gray6' : 'bg-orange2'}`}></hr>
+        <div
+          className={`flex h-10 w-10 items-center justify-center rounded-full border border-gray6 text-gray6 ${
+            step >= 2 && 'border-orange2 text-orange2'
+          }`}>
+          {step <= 2 ? 2 : <AiOutlineCheck />}
+        </div>
+        <hr className={`h-[1px] grow border-0 ${step < 3 ? 'bg-gray6' : 'bg-orange2'}`}></hr>
+        <div
+          className={`flex h-10 w-10 items-center justify-center rounded-full border border-gray6 text-gray6
+        ${step >= 3 && 'border-orange2 text-orange2'}`}>
+          {step <= 3 ? 3 : <AiOutlineCheck />}
+        </div>
       </div>
       <div className="gap-auto prose-subtitle-M flex w-[664px]">
         <span className="w-1/3 text-orange2">
           <p>Step.1</p>
           <p>글쓰기</p>
         </span>
-        <span className="w-1/3 text-center text-gray6">
+        <span className={`w-1/3 text-center text-gray6 ${step >= 2 && 'text-orange2'}`}>
           <p>Step.2</p>
           <p>글정보등록</p>
         </span>
-        <span className="w-1/3 text-right text-gray6">
+        <span className={`w-1/3 text-right text-gray6 ${step >= 3 && 'text-orange2'}`}>
           <p>Step.3</p>
           <p>미리보기</p>
         </span>
