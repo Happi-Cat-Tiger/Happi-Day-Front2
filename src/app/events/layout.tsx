15e9b6137f6a27f0350b1a139daf68c7813e3237
sungdongyoon <ehdtjd0906@naver.com>
2023-12-29 23:19:43 +0900

    ADD: 이벤트 게시글 작성 가이드 / 게시판 바로가기 레이아웃

--- src/app/events/layout.tsx
+++ src/app/events/layout.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import EventGuide from '@/containers/events/EventGuide';
+import Link from 'next/link';
 
 const layout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="m-auto h-screen border-2 border-black md:max-w-[1280px]">{children}</div>;
+  return (
+    <div className="m-auto h-screen md:max-w-[1280px]">
+      <EventGuide />
+      <div className="my-[60px] text-right">
+        <span className="prose-subtitle-M mr-[10px]">주최 관련 이벤트도 보고싶다면?</span>
+        <Link href="/board" className="h5">
+          주최 게시판 바로가기
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default layout;
