5ceb132bf6ce70041cf87316f8fe8af273455d53
Uwnag2 <126768997+jeonmye@users.noreply.github.com>
2023-12-29 18:29:28 +0900

    HDF-15 메뉴 기능, 반응형 구현

--- src/app/mypage/mySales/page.tsx
+++ src/app/mypage/mySales/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Page = () => {
   return (
     <div className="w-full">
-      <div className="mx-auto my-10 flex-row px-5"></div>
+      <div className="mx-auto my-10 flex px-5">나의 굿즈/공구 페이지</div>
     </div>
   );
 };
