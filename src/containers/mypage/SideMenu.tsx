11ca8a257f492c97aee71465416b2171ec3f14c9
Uwnag2 <126768997+jeonmye@users.noreply.github.com>
2023-12-29 20:28:45 +0900

    STYLE : 오타 수정 / 동적 클래스 수정 / url 케밥케이스로 변경

--- src/containers/mypage/SideMenu.tsx
+++ src/containers/mypage/SideMenu.tsx
@@ -5,10 +5,10 @@ import MenuItems from './MenuItems';
 
 const MenuList = [
   { id: 0, title: '프로필', path: 'profile' },
-  { id: 1, title: '나의 게시글', path: 'myPosts' },
-  { id: 2, title: '나의 굿즈', path: 'mySales' },
-  { id: 3, title: '나의 구독', path: 'mySubscribes' },
-  { id: 4, title: '나의 이벤트', path: 'myEvents' },
+  { id: 1, title: '나의 게시글', path: 'my-posts' },
+  { id: 2, title: '나의 굿즈', path: 'my-sales' },
+  { id: 3, title: '나의 구독', path: 'my-subscribes' },
+  { id: 4, title: '나의 이벤트', path: 'my-events' },
   { id: 5, title: '관리자 메뉴', path: 'admin' },
 ];
 
