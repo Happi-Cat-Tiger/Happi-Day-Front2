cde08b3c8af5ead43920430e8a95ff97553a960f
이지혜 <ehckehck@gmail.com>
2023-12-31 16:00:09 +0900

    FIX : comflict 해결

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,6 +8,7 @@ const Nav = () => {
   const navLink = ['/board', '/events', '/sales', '/auth/login'];
 
   return (
+<<<<<<< HEAD
     <div className="w-full border-b-2 border-orange2">
       <div className="m-auto flex h-[60px] w-[1024px] flex-row items-center justify-between">
         <Link href="/">
@@ -17,6 +18,17 @@ const Nav = () => {
           <ul className="flex flex-row gap-[50px]">
             {navList.map((el, idx) => (
               <li key={idx} className="prose-h6 cursor-pointer hover:text-orange2">
+=======
+    <div className="w-screen border-b-2 border-orange2">
+      <div className="m-auto flex h-[60px] w-[1024px] flex-row items-center justify-between">
+        <Link href="/">
+          <Image src={logo} alt="logo" className="h-[30px] w-[130px] cursor-pointer" />
+        </Link>
+        <div>
+          <ul className="flex flex-row gap-10">
+            {navList.map((el, idx) => (
+              <li key={idx} className="prose-h7 cursor-pointer hover:text-orange2">
+>>>>>>> b1d9b7e (FIX : comflict 해결)
                 <Link href={navLink[idx]}>{el}</Link>
               </li>
             ))}
