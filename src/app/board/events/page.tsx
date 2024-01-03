8670a37cf6347fd11542060ac2cc9a5981d349aa
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-03 21:18:52 +0900

    chore: 간단한 글자 수정, board page 수정

--- src/app/board/events/page.tsx
+++ src/app/board/events/page.tsx
@@ -2,22 +2,24 @@ import Card from '@/components/Card';
 
 export default function EventsPage() {
   return (
-    <div>
-      <Card
-        id={1}
-        cardType="events"
-        thumbnailUrl=""
-        title="카드"
-        artist="세븐틴"
-        location="대구"
-        startTime="2020"
-        endTime="2021"
-        address="지약"
-        joinMember={10}
-        likeCount={100}
-        commentCount={11}
-        viewCount={66}
-      />
+    <div className="grid grid-cols-2 place-items-center md:grid-cols-5">
+      {new Array(10).fill(0).map((v) => (
+        <Card
+          id={1}
+          cardType="events"
+          thumbnailUrl=""
+          title="카드"
+          artist="세븐틴"
+          location="장소"
+          startTime="2020"
+          endTime="2021"
+          address="지역"
+          joinMember={10}
+          likeCount={100}
+          commentCount={11}
+          viewCount={66}
+        />
+      ))}
     </div>
   );
 }
