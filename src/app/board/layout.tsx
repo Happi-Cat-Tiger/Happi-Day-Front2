a5b315740c1418f3450b28c2143ff23015c65a5a
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-03 21:18:52 +0900

    chore: 간단한 글자 수정, board page 수정

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -1,13 +1,10 @@
-'use client';
-
 import HorizontalLinkList from '@/components/List/HorizontalLinkList';
 import { BOARD_CATEGORY } from '@/constants/board';
 import Image from 'next/image';
 import SubBanner from 'public/images/subscriptionBanner.png';
-
 const BoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto my-[100px] flex h-full w-full flex-col items-center justify-center border-2 border-solid md:max-w-[1280px]">
+    <div className="mx-auto my-[60px] flex h-full w-full flex-col items-center justify-center md:max-w-[1280px]">
       <div className="flex flex-col gap-[60px]">
         <Image src={SubBanner} alt="구독 배너" className="h-auto w-screen" />
         <HorizontalLinkList category={BOARD_CATEGORY} />
