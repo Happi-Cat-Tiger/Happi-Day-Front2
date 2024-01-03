ea8250452b43ba08d40fd16bd68b7f13053bddbd
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-03 21:12:00 +0900

    chore: pathname에 맞으면 색상 변하게

--- src/components/List/HorizontalLinkList.tsx
+++ src/components/List/HorizontalLinkList.tsx
@@ -1,22 +1,26 @@
+'use client';
 import { CategoryNav } from '@/types/board';
 import Link from 'next/link';
 import React from 'react';
-
+import { usePathname } from 'next/navigation';
 interface HorizontalLinkListProps {
   category: CategoryNav[];
 }
 const HorizontalLinkList = ({ category }: HorizontalLinkListProps) => {
+  const pathname = usePathname();
+
   return (
     <div className="flex gap-2 md:gap-4">
-      {category.map((page, i) => {
-        return (
-          <Link key={i} href={page.navigate}>
-            <p className="prose-h6 w-fit border-b-2 border-gray6 p-2 text-[#545D69] md:prose-h4 hover:text-orange2">
-              {page.pageTitle}
-            </p>
-          </Link>
-        );
-      })}
+      {category.map((page, i) => (
+        <Link key={i} href={page.navigate}>
+          <p
+            className={`prose-h6 w-fit truncate border-b-2 p-2 md:prose-h4 hover:border-orange2 hover:text-orange2 ${
+              page.navigate === pathname ? 'border-orange1 text-orange1' : 'border-gray4 text-gray4'
+            }`}>
+            {page.pageTitle}
+          </p>
+        </Link>
+      ))}
     </div>
   );
 };
