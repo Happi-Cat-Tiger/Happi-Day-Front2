4f5fa61b30a0c063e097c5f1f61fe05eff2fd0d8
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-03 18:05:45 +0900

    feature: 게시판 카테고리

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -1,8 +1,18 @@
 'use client';
+
+import HorizontalLinkList from '@/components/List/HorizontalLinkList';
+import { BOARD_CATEGORY } from '@/constants/board';
+import Image from 'next/image';
+import SubBanner from 'public/images/subscriptionBanner.png';
+
 const BoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto my-[100px] flex h-full flex-col items-center justify-center border-2 border-solid px-2 py-10 md:max-w-[1280px]">
-      {children}
+    <div className="mx-auto my-[100px] flex h-full w-full flex-col items-center justify-center border-2 border-solid md:max-w-[1280px]">
+      <div className="flex flex-col gap-[60px]">
+        <Image src={SubBanner} alt="구독 배너" className="h-auto w-screen" />
+        <HorizontalLinkList category={BOARD_CATEGORY} />
+        {children}
+      </div>
     </div>
   );
 };
