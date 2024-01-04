fb7cd5cd8b3d43769011b56dde902cbefb0cb845
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-04 21:43:55 +0900

    -chore: key값

--- src/app/board/events/page.tsx
+++ src/app/board/events/page.tsx
@@ -3,8 +3,9 @@ import Card from '@/components/Card';
 export default function EventsPage() {
   return (
     <div className="grid grid-cols-2 place-items-center md:grid-cols-5">
-      {new Array(10).fill(0).map((v) => (
+      {new Array(10).fill(0).map((v, i) => (
         <Card
+          key={i}
           id={1}
           cardType="events"
           thumbnailUrl=""
--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -6,7 +6,7 @@ const BoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mx-auto my-[60px] flex h-full w-full flex-col items-center justify-center md:max-w-[1280px]">
       <div className="flex w-full flex-col gap-[60px] px-2 md:px-0">
-        <Image src={SubBanner} alt="구독 배너" className="h-auto w-screen" />
+        <Image src={SubBanner} alt="구독 배너" className="h-auto w-screen" priority />
         <HorizontalLinkList category={BOARD_CATEGORY} />
         {children}
       </div>
