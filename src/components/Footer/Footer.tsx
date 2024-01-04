3531660b932a250990c110009e98c1c0bb387811
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-05 01:06:36 +0900

    chore: footer 반응형

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import { FaFacebookF } from 'react-icons/fa6';
 export default function Footer() {
   return (
     <div className="my-10 flex w-full shrink-0 flex-col items-center justify-end rounded-[2px] border-t-[1px] border-orange3 text-center text-xs font-medium leading-[150%] text-gray2">
-      <div className="flex w-[1024px] flex-col gap-[25px] pt-[25px]">
+      <div className="flex w-full flex-col gap-[25px] pt-[25px] md:max-w-[1280px]">
         <div className="flex items-start justify-center gap-[20px] md:gap-[100px]">
           <div>이용약관</div>
           <div>개인정보취급방침</div>
