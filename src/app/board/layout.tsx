816d606b4791008400c5a2fc744805d1c81d981e
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-04 18:35:07 +0900

    chore: 반응형 list

--- src/app/board/layout.tsx
+++ src/app/board/layout.tsx
@@ -5,7 +5,7 @@ import SubBanner from 'public/images/subscriptionBanner.png';
 const BoardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="mx-auto my-[60px] flex h-full w-full flex-col items-center justify-center md:max-w-[1280px]">
-      <div className="flex flex-col gap-[60px]">
+      <div className="flex w-full flex-col gap-[60px] px-2 md:px-0">
         <Image src={SubBanner} alt="구독 배너" className="h-auto w-screen" />
         <HorizontalLinkList category={BOARD_CATEGORY} />
         {children}
