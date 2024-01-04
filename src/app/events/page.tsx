e41760c26811d9c5953df0d8804534266cba0834
sungdongyoon <ehdtjd0906@naver.com>
2024-01-04 21:07:38 +0900

    CHORE: 페이지네이션 스타일 수정정

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react';
 import Card from '@/components/Card';
-import Pagination from 'react-js-pagination';
 import PaginationComponent from '@/components/Pagenation/PaginationComponent';
 import '../../styles/global.css';
 
