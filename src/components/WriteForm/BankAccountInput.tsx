7afbf8585292398f5caa50147a60c6cf250a3c0f
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-06 01:00:45 +0900

    chore: mobile size 조정

--- src/components/WriteForm/BankAccountInput.tsx
+++ src/components/WriteForm/BankAccountInput.tsx
@@ -3,24 +3,24 @@ import React from 'react';
 const BankAccountInput = () => {
   return (
     <div className="flex flex-col gap-2">
-      <div className=" prose-h5">
+      <div className="prose-h6 md:prose-h5">
         <span>계좌 등록</span> <span className="text-red-600">*</span>
       </div>
       <div className="flex gap-2">
         <input
           type="text"
-          className="prose-body-S w-[300px] rounded-md border border-gray3 px-3 py-3 focus:border-orange1 focus:outline-none "
+          className="prose-body-XS w-[300px] rounded-md border border-gray3 px-3 py-3 md:prose-body-S focus:border-orange1 focus:outline-none "
           placeholder="은행 이름"
         />
         <input
           type="text"
-          className="prose-body-S w-[100px] rounded-md border border-gray3 px-3 py-3 focus:border-orange1 focus:outline-none "
+          className="prose-body-XS w-[100px] rounded-md border border-gray3 px-3 py-3 md:prose-body-S focus:border-orange1 focus:outline-none "
           placeholder="예금주명"
         />
       </div>
       <input
         type="text"
-        className="prose-body-S w-full rounded-md border border-gray3 px-3 py-3 focus:border-orange1 focus:outline-none "
+        className="prose-body-XS w-full rounded-md border border-gray3 px-3 py-3 md:prose-body-S focus:border-orange1 focus:outline-none "
         placeholder={`계좌번호 ("-" 제외하고 숫자만 입력)`}
       />
     </div>
--- src/components/WriteForm/HashtagInput.tsx
+++ src/components/WriteForm/HashtagInput.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 const HashtagInput = () => {
   return (
     <div className="flex flex-col gap-2">
-      <div className=" prose-h5">
+      <div className="prose-h6 md:prose-h5">
         <span>해시태그 등록</span> <span className="text-red-600">*</span>
       </div>
       <input
         type="text"
-        className="prose-body-S w-full rounded-md border border-gray3 px-3 py-3 focus:border-orange1 focus:outline-none "
+        className="prose-body-XS w-full rounded-md border border-gray3 px-3 py-3 md:prose-body-S focus:border-orange1 focus:outline-none "
         placeholder="#해시태그를 입력해주세요 ex) #해시태그"
       />
     </div>
