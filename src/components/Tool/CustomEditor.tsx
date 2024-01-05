7e4e9048e65045d10827b06e64feebafaa0ae75e
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-06 01:22:53 +0900

    add: constant/editor.tsx chore: state값 관리

--- src/components/Tool/CustomEditor.tsx
+++ src/components/Tool/CustomEditor.tsx
@@ -1,49 +1,23 @@
 'use client';
-import React, { useState, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import dynamic from 'next/dynamic';
 import 'react-quill/dist/quill.snow.css';
+import { QUILL_EDITOR } from '@/constants/editor';
 
-const CustomEditor = ({ initialData }: { initialData: string }) => {
+interface CustomEditorProps {
+  editValue: string;
+  setEditValue: React.Dispatch<React.SetStateAction<string>>;
+}
+const CustomEditor = ({ editValue, setEditValue }: CustomEditorProps) => {
   const ReactQuill = useMemo(() => dynamic(() => import('react-quill'), { ssr: false }), []);
 
-  const [editValue, setEditValue] = useState(initialData);
-
-  const modules = {
-    toolbar: [
-      ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-
-      [{ header: 1 }, { header: 2 }], // custom button values
-      [{ header: [1, 2, 3, 4, 5, 6, false] }], // font size
-      [{ list: 'ordered' }, { list: 'bullet' }],
-
-      [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-      [{ align: [] }],
-      ['link', 'image'],
-    ],
-  };
-
-  const formats = [
-    'header',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'list',
-    'bullet',
-    'link',
-    'image',
-    'color',
-    'background',
-    'size',
-    'align',
-  ];
   return (
     <ReactQuill
       theme="snow"
       defaultValue={editValue}
       onChange={setEditValue}
-      modules={modules}
-      formats={formats}
+      modules={QUILL_EDITOR.modules}
+      formats={QUILL_EDITOR.formats}
       className="h-full w-full overflow-auto"
     />
   );
