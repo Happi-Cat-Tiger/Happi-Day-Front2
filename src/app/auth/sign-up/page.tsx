b2480514b8255bdbfe5694f50364d8e7eeafba68
이지혜 <ehckehck@gmail.com>
2024-01-09 22:00:50 +0900

    ADD : sign-up fuction 추가

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,25 +1,22 @@
+'use client';
 import AuthInput from '@/components/Auth/AuthInput';
-import SubmitButton from '@/components/Button/SubmitButton';
 import { AiOutlineUser, AiOutlineLock, AiOutlinePhone } from 'react-icons/ai';
 
 const signUp = () => {
   const inputPlaceHolder = [
-    { title: '아이디', icon: AiOutlineUser, content: 'example@happiday.com' },
-    { title: '비밀번호', icon: AiOutlineLock, content: '********' },
-    { title: '비밀번호 확인', icon: AiOutlineLock, content: '********' },
-    { title: '이름', icon: AiOutlineUser, content: '홍길동' },
-    { title: '휴대폰 번호', icon: AiOutlinePhone, content: '000-0000-0000' },
-    { title: '닉네임', icon: AiOutlineUser, content: 'ex)해피데이' },
+    { label: '아이디', name: 'id', type: 'text', icon: AiOutlineUser, content: 'example@happiday.com' },
+    { label: '비밀번호', name: 'pw', type: 'password', icon: AiOutlineLock, content: '********' },
+    { label: '비밀번호 확인', name: 'pwConfirm', type: 'password', icon: AiOutlineLock, content: '********' },
+    { label: '이름', name: 'name', type: 'text', icon: AiOutlineUser, content: '홍길동' },
+    { label: '휴대폰 번호', name: 'phoneNumber', type: 'text', icon: AiOutlinePhone, content: '01012345678' },
+    { label: '닉네임', name: 'nickName', type: 'text', icon: AiOutlineUser, content: 'ex)해피데이' },
   ];
-
-  const handleButtonClick = {};
 
   return (
     <>
-      <div className="py-10 px-4 gap-10 m-auto flex w-[360px] flex-col items-center md:w-[476px] md:gap-[60px] md:py-[60px]">
+      <div className="m-auto flex w-[360px] flex-col items-center gap-10 px-4 py-10 md:w-[476px] md:gap-[60px] md:py-[60px]">
         <div className="prose-h4 text-gray1 md:prose-h3">회원가입</div>
         <AuthInput inputPlaceHolder={inputPlaceHolder} />
-        <SubmitButton label="가입하기" disabled={true} onClick={handleButtonClick} />
       </div>
     </>
   );
