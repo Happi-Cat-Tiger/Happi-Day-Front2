448b06fc0efbb1be9dfe1fa285034a59a05f55dc
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-11 22:38:11 +0900

    fix: horizontalLinkList : 첫link default

--- src/app/board/all/page.tsx
+++ src/app/board/all/page.tsx
@@ -1,18 +1,31 @@
+'use client';
 import ArticleList from '@/components/List/ArticleList';
 import LinkButton from '@/components/Button/LinkButton';
 import Link from 'next/link';
+import PaginationComponent from '@/components/Pagination/PaginationComponent';
+import { useState } from 'react';
 
 export default function AllPage() {
+  const [page, setPage] = useState(1);
+  const postPerPage = 10;
+  const indexOfLastPost = page * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+
+  const pageChange = (page: number) => {
+    setPage(page);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div>
-        <ArticleList />
-        <ArticleList />
-        <ArticleList />
-        <ArticleList />
-        <ArticleList />
-        <ArticleList />
+        {new Array(20)
+          .fill(0)
+          .slice(indexOfFirstPost, indexOfLastPost)
+          .map((v, i) => (
+            <ArticleList />
+          ))}
       </div>
+      <PaginationComponent page={page} totalItemsCount={20} pageChange={pageChange} />
       <div className="flex justify-end">
         <Link href="/board/write" passHref legacyBehavior>
           <LinkButton
