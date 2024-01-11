c13fe6cd85eb725d878b5d3a712f584bd1b24646
이지혜 <ehckehck@gmail.com>
2024-01-11 20:13:46 +0900

    CHORE : sign-in tailwind 수정

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,30 +1,26 @@
 'use client';
 import AuthInput from '@/components/Auth/AuthInput';
-import LoginButton from '@/components/Button/LoginButton';
 import { AiOutlineUser, AiOutlineLock } from 'react-icons/ai';
 
 const SignIn = () => {
   const inputPlaceHolder = [
-    { name: 'id', label: '', icon: AiOutlineUser, type: 'text', content: 'example@happiday.com' },
-    { name: 'password', label: '', type: 'password', icon: AiOutlineLock, content: '비밀번호' },
+    { label: '', type: 'string', name: 'id', icon: AiOutlineUser, content: 'example@happiday.com' },
+    { label: '', type: 'string', name: 'pw', icon: AiOutlineLock, content: '비밀번호' },
   ];
-
-  const handleButtonClick = () => {};
 
   return (
     <>
-      <div className="pt-20 px-2 md:gap-10 m-auto flex w-[360px] flex-col items-center gap-[37px] pb-[130px] md:py-[120px]">
+      <div className="m-auto flex w-[360px] flex-col items-center gap-[37px] px-2 pb-[130px] pt-20 md:gap-10 md:py-[120px]">
         <div className="prose-h4 text-gray1 md:prose-h3">로그인</div>
         <AuthInput inputPlaceHolder={inputPlaceHolder} />
-        <div className="md:gap-10 flex flex-col items-center gap-[33px] self-stretch">
-          <div className="gap-3 prose-body-XS flex items-center text-gray1">
+        <div className="flex flex-col items-center gap-[33px] self-stretch md:gap-10">
+          <div className="prose-body-XS flex items-center gap-3 text-gray1">
             <div>이메일 찾기</div>
             <div>|</div>
             <div>비밀번호 찾기</div>
             <div>|</div>
             <div>회원가입</div>
           </div>
-          <LoginButton disabled={false} onClick={handleButtonClick} />
         </div>
       </div>
     </>
