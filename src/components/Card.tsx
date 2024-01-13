6ef1b4be2e4a16f2106fb7169f45c1b8e92c3f14
sungdongyoon <ehdtjd0906@naver.com>
2024-01-13 23:56:17 +0900

    Feature: 메인페이지 슬라이더 구현

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -42,7 +42,7 @@ const Card = ({
       key={id}
       onClick={() => router.push(`${cardType}/${id}`)}
       className="flex h-[300px] w-[224px] cursor-pointer flex-col gap-[12px] p-[12px] shadow-lg">
-      {/*next.config.js에서 모듈 이미지 경로 설정해야함 ! */}
+      {/*임시로 넣은 이미지는 next.config.js에서 모듈 이미지 경로 설정해야함*/}
       <Image src={thumbnailUrl} width={`${200}`} height={140} alt="thumbnail" className="flex-1 rounded-[4px]" />
       <div className="flex flex-1 flex-col items-center justify-center gap-1 border-t-[1px] border-black">
         <span className="prose-h6">{title}</span>
