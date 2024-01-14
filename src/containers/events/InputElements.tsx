28d5947feb6d464d516fe6254301ca635e7c0038
sungdongyoon <ehdtjd0906@naver.com>
2024-01-14 17:29:48 +0900

    CHORE: 이벤트 카드형 게시판 input 텍스트 수정

--- src/containers/events/InputElements.tsx
+++ src/containers/events/InputElements.tsx
@@ -20,21 +20,21 @@ const InputElements = () => {
     <div className="my-[60px] flex justify-between sm:flex-col sm:gap-[16px] md:flex-row md:items-center md:gap-[0]">
       <div className="flex items-center sm:justify-between md:gap-[24px]">
         <div>
-          <select className="cursor-pointer rounded-[16px] bg-[#F0F5F9] px-[16px] py-[8px] outline-none">
-            <option className="prose-btn-M">최신순</option>
-            <option className="prose-btn-M">오래된순</option>
+          <select className="cursor-pointer rounded-[16px] bg-[#F0F5F9] px-[16px] py-[8px] outline-none sm:prose-btn-S md:prose-btn-M">
+            <option>최신순</option>
+            <option>오래된순</option>
           </select>
         </div>
         <div className="flex flex-col items-start gap-1">
           <div className="flex justify-center gap-2">
             <input id="subscribe" type="checkbox" />
-            <label htmlFor="subscribe" className="prose-subtitle-M">
+            <label htmlFor="subscribe" className="sm:prose-body-XS md:prose-body-M">
               내가 구독한 아티스트만 보기
             </label>
           </div>
           <div className="flex justify-center gap-2">
             <input id="ongoing" type="checkbox" />
-            <label htmlFor="ongoing" className="prose-subtitle-M">
+            <label htmlFor="ongoing" className="sm:prose-body-XS md:prose-body-M">
               진행중인 이벤트만 보기
             </label>
           </div>
