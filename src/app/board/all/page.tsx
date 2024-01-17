47b7b7caf54abd40a1bea04f2fa94f90bde4d3cb
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-17 19:09:29 +0900

    chore: paginaiton countPerPage 입력

--- src/app/board/all/page.tsx
+++ src/app/board/all/page.tsx
@@ -30,7 +30,7 @@ export default function AllPage() {
             <ArticleList key={i} />
           ))}
       </div>
-      <PaginationComponent page={page} totalItemsCount={20} pageChange={pageChange} />
+      <PaginationComponent countPerPage={postPerPage} page={page} totalItemsCount={20} pageChange={pageChange} />
       <div className="flex justify-end">
         <Link href="/board/write" passHref legacyBehavior>
           <LinkButton
