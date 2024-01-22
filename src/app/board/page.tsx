a62aebad032c164cc93c84f2a6d2eb39bfdec775
성은지 <96868722+eunji1@users.noreply.github.com>
2024-01-22 14:39:48 +0900

    - chore: delete error code

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,12 +1,10 @@
 'use client';
-import { useEffect } from 'react';
 import AllPage from './all/page';
 
 const BoardPage = () => {
-  useEffect(() => {
-    throw new Error('테스트용 에러 발생!');
-    // 이하 코드는 실행되지 않음
-  }, []);
+  // useEffect(() => {
+  //   throw new Error('테스트용 에러 발생!');
+  // }, []);
 
   return <AllPage />;
 };
