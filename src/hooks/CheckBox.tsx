4db91822bb43a5cd0df03777d6ce69b7180de863
이지혜 <ehckehck@gmail.com>
2024-01-25 20:07:26 +0900

    FIX : comflict 해결

--- src/hooks/CheckBox.tsx
+++ src/hooks/CheckBox.tsx
@@ -24,15 +24,9 @@ const CheckBox = ({ checkBoxData, handleAgreementChange, toggleAllAgreements }: 
   return (
     <div key={id} className="flex cursor-pointer items-center gap-2" onClick={handleClick}>
       {isChecked ? (
-<<<<<<< HEAD
-        <IoCheckbox className="h-6 w-6 text-orange2" />
-      ) : (
-        <IoCheckboxOutline className="h-6 w-6 text-orange2" />
-=======
         <IoCheckbox className="h-6 w-6 absolute text-orange2" />
       ) : (
         <IoCheckboxOutline className="h-6 w-6 absolute text-orange2" />
->>>>>>> 1174826 (CHORE : 체크박스 수정중)
       )}
 
       <div className="prose-subtitle-M text-black md:prose-subtitle-L">{title}</div>
