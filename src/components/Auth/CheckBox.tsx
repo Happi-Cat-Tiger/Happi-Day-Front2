08a397238dd2300593ab43b58ecedf679f3845fc
이지혜 <ehckehck@gmail.com>
2024-01-25 20:22:39 +0900

    ADD : terms 기능

---
+++ src/components/Auth/CheckBox.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { IoCheckboxOutline, IoCheckbox } from 'react-icons/io5';
+
+interface CheckBoxData {
+  id: number;
+  title: string;
+  content: string;
+  isChecked: boolean;
+}
+
+interface CheckBoxProps {
+  checkBoxData: CheckBoxData;
+  handleAgreementChange: (id: number, isChecked: boolean) => void;
+  toggleAllAgreements: (isChecked: boolean) => void;
+}
+
+const CheckBox = ({ checkBoxData, handleAgreementChange, toggleAllAgreements }: CheckBoxProps) => {
+  const { id, title, isChecked } = checkBoxData;
+
+  const handleClick = () => {
+    id !== 1 ? handleAgreementChange(id, !isChecked) : toggleAllAgreements(!isChecked);
+  };
+
+  return (
+    <div key={id} className="flex cursor-pointer items-center gap-2" onClick={handleClick}>
+      {isChecked ? (
+        <IoCheckbox className="h-6 w-6 text-orange2" />
+      ) : (
+        <IoCheckboxOutline className="h-6 w-6 text-orange2" />
+      )}
+
+      <div className="prose-subtitle-M text-black md:prose-subtitle-L">{title}</div>
+    </div>
+  );
+};
+
+export default CheckBox;
