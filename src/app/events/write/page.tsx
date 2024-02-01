5180eff71854dd45c2f2aec2898e984a1a021cd7
sungdongyoon <ehdtjd0906@naver.com>
2024-02-01 21:58:51 +0900

    .

--- src/app/events/write/page.tsx
+++ src/app/events/write/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import StepProgressBar from '@/components/Bar/StepProgressBar';
-import PrimaryButton from '@/components/Button/PrimaryButton';
+import StyledButton from '@/components/Button/StyledButton';
 import EventsWritingStep from '@/containers/events/EventsWritingStep';
 import EventsWritingInfoStep from '@/containers/events/EventsWritingInfoStep';
 import EventsPreviewWringStep from '@/containers/events/EventsPreviewWringStep';
@@ -8,13 +8,22 @@ import React, { useState } from 'react';
 
 const page = () => {
   const [step, setStep] = useState<number>(1);
+
+  const onDisable = () => {};
   return (
     <section className="mx-auto flex h-full w-full flex-col items-center justify-center gap-4 md:max-w-[996px]">
       <StepProgressBar step={step} />
       <div className="flex w-full justify-between">
-        <PrimaryButton label="이전" disabled={step <= 1} onClick={() => setStep(step - 1)} />
-        <PrimaryButton
+        <StyledButton
+          className="prose-btn-M rounded-2xl bg-[#E85ECF] px-5 py-3 text-white md:prose-btn-L hover:bg-pink2 focus:outline-none disabled:bg-gray6 md:px-6 md:py-4"
+          label="이전"
+          disabled={step <= 1}
+          onClick={() => setStep(step - 1)}
+        />
+        <StyledButton
+          className="prose-btn-M rounded-2xl bg-[#E85ECF] px-5 py-3 text-white md:prose-btn-L hover:bg-pink2 focus:outline-none disabled:bg-gray6 md:px-6 md:py-4"
           label={step === 3 ? '완료' : '다음'}
+          // disabled={onDisable()}
           onClick={() => {
             if (step === 3) {
               alert('글 작성이 완료되었습니다');
