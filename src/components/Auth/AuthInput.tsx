4d7b57073c4e67de58fc0cc6095311dcc3b3af06
성은지 <96868722+eunji1@users.noreply.github.com>
2024-02-01 15:33:47 +0900

    - refactor: 코드정리, 로그인 api

--- src/components/Auth/AuthInput.tsx
+++ src/components/Auth/AuthInput.tsx
@@ -1,129 +1,114 @@
 import React from 'react';
+import { AuthInputPlaceholder } from '@/types/auth';
 import { useForm } from 'react-hook-form';
-
-interface InputPlaceholder {
-  label: string;
-  type: string;
-  icon: React.ElementType;
-  content: string;
-  name: string;
-}
+import { AUTH_REACT_HOOK_FORM } from '@/constants/auth';
+import useSignInMutation from '@/services/useSignInMutation';
 
 interface AuthInputProps {
-  inputPlaceHolder: InputPlaceholder[];
+  inputPlaceHolder: AuthInputPlaceholder[];
 }
 
-interface FormData {
+type LoginFormValues = {
+  id: string;
+  pw: string;
+};
+
+type SignupFormValues = {
+  id: string;
+  name: string;
+  nickname: string;
+  phoneNumber: string;
+  pw: string;
+  pwConfirm: string;
+};
+
+type FormValues = {
   [key: string]: string;
-}
+};
 
 const AuthInput = ({ inputPlaceHolder }: AuthInputProps) => {
+  const mutation = useSignInMutation();
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors, dirtyFields },
-  } = useForm<FormData>({ mode: 'onBlur' });
+  } = useForm<FormValues>({ mode: 'onBlur' });
 
-  const onSubmit = (data: FormData) => {
-    console.log(data);
-  };
+  const onSubmit = handleSubmit((data) => {
+    // 회원가입 페이지에서의 api 동작 x
+    if (inputPlaceHolder.length === 6) return;
+    // 로그인 api
+    mutation.mutate({ username: data.id, password: data.pw });
+  });
 
-  const isFormValid = Object.keys(errors).length === 0 && Object.keys(dirtyFields).length === inputPlaceHolder.length;
-  const isPhoneNumberField = (name: string) => {
+  const isFormValid: boolean =
+    Object.keys(errors).length === 0 && Object.keys(dirtyFields).length === inputPlaceHolder.length;
+  const isHaveLabel: boolean = inputPlaceHolder.some((item) => item.label);
+  const isPhoneNumberField = (name: string): boolean => {
     return name === 'phoneNumber';
   };
-  const haveLabel = inputPlaceHolder.some((item) => item.label);
   const watchPw = watch('pw', '');
   const checkPasswordConfirmation = (value: string) => {
     return value === watchPw || '비밀번호가 일치하지 않습니다.';
   };
 
+  const getPattern = (name: string) => AUTH_REACT_HOOK_FORM.VALIDATION_PATTERNS[name] || null;
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10 self-stretch md:gap-[60px]">
+    <form onSubmit={onSubmit} className="flex flex-col gap-10 self-stretch md:gap-[60px]">
       <div>
-        {inputPlaceHolder.map((item, index) => (
-          <div
-            key={index}
-            className={`mb-1 flex w-full items-center md:mb-3 justify-${
-              item.label ? 'between' : 'center'
-            } self-stretch`}>
-            {item.label && <div className="prose-subtitle-S text-gray1 md:prose-subtitle-M">{item.label}</div>}
-            <div className="flex flex-col">
-              <div
-                className={`border-[1px]-${
-                  errors[item.name] ? 'red-500' : 'gray-5'
-                } flex h-[44px] items-center gap-2.5 rounded-lg border-[1px] px-2 py-2.5 ${
-                  item.label ? 'w-[200px] md:w-[300px]' : 'w-[300px]'
-                }`}>
-                {React.createElement(item.icon, {
-                  className: `w-6 h-6 flex-shrink-0 text-${errors[item.name] ? 'red-500' : 'gray5'}`,
-                })}
-                <input
-                  {...register(item.name, {
-                    required: true,
-                    ...(item.name === 'id' && {
-                      pattern: {
-                        value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/,
-                        message: `• 올바른 이메일 형식이 아닙니다.`,
-                      },
-                    }),
-                    ...(item.name === 'pw' && {
-                      pattern: {
-                        value: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+\\\|\[\]{};:\'",.<>/?]).{8,}$/,
-                        message: '• 영문, 숫자, 특수문자 포함 8글자 이상이어야 합니다.',
-                      },
-                    }),
-                    ...(item.name === 'pwConfirm' && {
-                      validate: checkPasswordConfirmation,
-                    }),
-                    ...(item.name === 'name' && {
-                      pattern: {
-                        value: /^[A-Za-z가-힣]+$/,
-                        message: '• 영문 또는 한글만 입력해주세요.',
-                      },
-                    }),
-                    ...(item.name === 'phoneNumber' && {
-                      pattern: {
-                        value: /^[0-9]{1,11}$/,
-                        message: '• 하이픈(-)을 제외한 숫자만 입력해주세요.',
-                      },
-                    }),
-                    ...(item.name === 'nickName' && {
-                      pattern: {
-                        value: /^[A-Za-z가-힣]{1,10}$/,
-                        message: '• 영문 또는 한글로 10글자 이내로 입력해주세요.',
-                      },
-                    }),
+        {inputPlaceHolder.map((item, index) => {
+          const getValidation = (item: any) => {
+            if (item.name === 'pwConfirm') return { validate: checkPasswordConfirmation };
+            const pattern = getPattern(item.name);
+            return pattern ? { pattern } : null;
+          };
+          return (
+            <div
+              key={index}
+              className={`mb-1 flex w-full items-center md:mb-3 ${
+                item.label ? 'justify-between' : 'justify-center'
+              } self-stretch`}>
+              {item.label && <div className="prose-subtitle-S text-gray1 md:prose-subtitle-M">{item.label}</div>}
+              <div className="flex flex-col">
+                <div
+                  className={`${
+                    errors[item.name] ? 'border-red-500' : 'border-gray-5'
+                  } flex h-[44px] items-center gap-2.5 rounded-lg border-[1px] px-2 py-2.5 ${
+                    item.label ? 'w-[200px] md:w-[300px]' : 'w-[300px]'
+                  }`}>
+                  {React.createElement(item.icon, {
+                    className: `w-6 h-6 flex-shrink-0 ${errors[item.name] ? 'text-red-500' : 'text-gray5'}`,
                   })}
-                  type={item.type}
-                  className={`${item.label ? 'prose-body-XS md:prose-body-S' : 'prose-body-S'} outline-none`}
-                  placeholder={item.content}
-                  maxLength={isPhoneNumberField(item.name) ? 11 : undefined}></input>
+                  <input
+                    {...register(item.name, {
+                      required: true,
+                      ...getValidation(item),
+                    })}
+                    type={item.type}
+                    className={`${item.label ? 'prose-body-XS md:prose-body-S' : 'prose-body-S'} outline-none`}
+                    placeholder={item.content}
+                    maxLength={isPhoneNumberField(item.name) ? 11 : undefined}
+                  />
+                </div>
+                {errors[item.name] && (
+                  <div className="prose-body-XXS text-red-500 md:prose-body-XS">
+                    &bull; {errors[item.name]?.message as string}
+                  </div>
+                )}
               </div>
-              {errors[item.name] && (
-                <div className="prose-body-XXS text-red-500 md:prose-body-XS">{errors[item.name]?.message}</div>
-              )}
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
       <div className="flex justify-center">
-        {haveLabel ? (
-          <button
-            type="submit"
-            disabled={!isFormValid}
-            className="prose-btn-M w-[300px] rounded-lg bg-orange2 py-3 text-white md:prose-btn-L hover:bg-orange1 disabled:bg-gray6">
-            가입하기
-          </button>
-        ) : (
-          <button
-            type="submit"
-            disabled={!isFormValid}
-            className="prose-btn-M w-[300px] rounded-lg bg-orange2 py-3 text-white md:prose-btn-L hover:bg-orange1 disabled:bg-gray6">
-            로그인
-          </button>
-        )}
+        <input
+          type="submit"
+          value={isHaveLabel ? '로그인' : '가입하기'}
+          disabled={!isFormValid}
+          className="prose-btn-M w-[300px] rounded-lg bg-orange2 py-3 text-white md:prose-btn-L hover:cursor-pointer hover:bg-orange1 disabled:bg-gray6"
+        />
       </div>
     </form>
   );
