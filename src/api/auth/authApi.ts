ad1f849a95a650d5800907c5985b61ad4e9f9644
성은지 <96868722+eunji1@users.noreply.github.com>
2024-02-11 23:00:29 +0900

    hotFix : 로그인 postData 수정

--- src/api/auth/authApi.ts
+++ src/api/auth/authApi.ts
@@ -1,6 +1,6 @@
 import apiInstance from '@/api/api';
 
 export const postSigninApi = async ({ username, password }: { username: string; password: string }) =>
-  await apiInstance.post('/auth/login', [, { username, password }]);
+  await apiInstance.post('/auth/login', { username, password });
 
 export const getSignoutApi = async () => await apiInstance.get('/auth/logout');
