89ddc0363dcc0c2c432e94172c03d9d2fd3bf7aa
성은지 <96868722+eunji1@users.noreply.github.com>
2024-02-21 17:14:06 +0900

    add : image 도메인 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {};
 
 module.exports = {
   images: {
-    domains: ['www.fitpetmall.com', 'blog.kakaocdn.net', 'ichef.bbci.co.uk', 't1.daumcdn.net', 'img1.daumcdn.net'],
+    domains: [
+      'localhost',
+      '*',
+      'happi-day.s3.ap-northeast-2.amazonaws.com',
+      'www.fitpetmall.com',
+      'blog.kakaocdn.net',
+      'ichef.bbci.co.uk',
+      't1.daumcdn.net',
+      'img1.daumcdn.net',
+    ],
   },
 };
