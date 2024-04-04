fe69ba6ebc8c7f40509403d9166e1d9c715917f5
sungdongyoon <ehdtjd0906@naver.com>
2024-04-04 20:05:49 +0900

    이벤트 데이터 post 작업중

--- src/api/events/eventsApi.ts
+++ src/api/events/eventsApi.ts
@@ -14,45 +14,31 @@ export const postEventsWriteApi = async ({
   imageFile,
 }: EventsWritePayload) => {
   const formData = new FormData();
-  const articleJson = new Blob(
-    [
-      JSON.stringify({
-        title: title,
-        startTime: startTime,
-        endTime: endTime,
-        description: description,
-        address: address,
-        location: location,
-        hashtags: hashtags,
-      }),
-    ],
-    {
-      type: 'application/json',
-    },
-  );
+
+  // 필요한 필드를 FormData에 추가
   formData.append('title', title);
   formData.append('description', description);
   formData.append('address', address);
   formData.append('location', location);
   formData.append('hashtags', JSON.stringify(hashtags));
-  formData.append('thumbnailFile', thumbnailFile || ''); // 파일 또는 빈 문자열
-  formData.append('imageFile', imageFile || ''); // 파일 또는 빈 문자열
 
-  // Date 객체를 문자열로 변환하여 append
+  thumbnailFile && formData.append('thumbnailFile', thumbnailFile);
+  imageFile && formData.append('imageFile', imageFile);
+
+  // Date 객체를 ISO 문자열로 변환하여 추가
   formData.append('startTime', startTime ? startTime.toISOString() : '');
   formData.append('endTime', endTime ? endTime.toISOString() : '');
-  // imageUrl && imageUrl.forEach((file) => formData.append('imageFile', file));
 
-  return await apiInstance
-    .post(`/events`, formData, {
+  try {
+    const response = await apiInstance.post(`/events`, formData, {
       headers: { 'Content-Type': 'multipart/form-data', accept: 'application/json' },
-      transformRequest: [
-        function () {
-          return formData;
-        },
-      ],
-    })
-    .then((response) => response.data);
+    });
+    return response.data;
+  } catch (error) {
+    // 오류 처리
+    console.error('Error:', error);
+    throw error;
+  }
 };
 
 // 이벤트 전체 조회
