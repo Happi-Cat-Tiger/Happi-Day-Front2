f8472984e6fe1e252753fd6fa1baf128c970ca65
성은지 <96868722+eunji1@users.noreply.github.com>
2024-04-28 20:22:48 +0900

    hotfix

--- next.config.js
+++ next.config.js
@@ -4,14 +4,13 @@ const nextConfig = {};
 module.exports = {
   images: {
     domains: [
-      'happi-day.s3.ap-northeast-2.amazonaws.com',
       'www.fitpetmall.com',
       'blog.kakaocdn.net',
       'ichef.bbci.co.uk',
       't1.daumcdn.net',
       'img1.daumcdn.net',
       'source.unsplash.com',
-      'happi-day.s3.ap-northeast-2.amazonaws.com',
+      'happi-day-bucket.s3.ap-northeast-2.amazonaws.com',
     ],
   },
 };
